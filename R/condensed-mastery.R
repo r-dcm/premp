758c4a5ba44cadf1c5b20c9ccf259c8e679b774f
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2026-01-26 15:35:10 -0500

    flesh out function for condensed mastery method

--- R/condensed-mastery.R
+++ R/condensed-mastery.R
@@ -4,25 +4,48 @@
 #' the condensed mastery standard setting method.
 #'
 #' @param ratings A tibble with one row for each rater's rating of an assigned
-#' profile.
+#' profile, including columns for the profile number, the mastery status for
+#' each attribute, and the raters' ratings.
 #' @param profiles A tibble with one row for each attribute mastery
-#' profiles that is eligible for assignment to raters.
+#' profiles that is eligible for assignment to raters. The rows correspond to
+#' the profile number in the `ratings` argument.
+#' @pl_labels A character vector containing the ordered performance levels.
+#' @att_levels An integer describing the number of categorical mastery classes
+#' for each attribute.
+#' @output_dir The output directory for the pinpointing ranges.
 #'
 #' @return [tibble][tibble::tibble-package] A tibble containing the pinpoint
 #' ranges to assign profiles during the second round of standard setting.
 #'
 #' @export
 #' @examples
-condensed_mastery <- function(ratings, profiles) {
+condensed_mastery <- function(
+  ratings,
+  profiles,
+  pl_labels,
+  att_levels,
+  output_dir
+) {
+  if (length(pl_labels) < 2) {
+    rdcmchecks::abort_bad_argument(
+      arg = rlang::caller_arg(pl_labels),
+      must = cli::format_message(paste(
+        "must have a value of at least 2."
+      ))
+    )
+  }
+
   # code adapted from https://github.com/atlas-aai/standard-setting/blob/main/01-pinpoint-ranges.R
   pinpoint_interval <- 0.89
   min_pinpoint_range <- 3
+
+  att_levels <- att_levels - 1
 
   # set options
   cmdstan_v <- cmdstanr::cmdstan_version(error_on_NA = FALSE)
   options(brms.backend = ifelse(is.null(cmdstan_v), "rstan", "cmdstanr"))
 
-  ee_vec <- profiles |>
+  att_vec <- profiles |>
     names()
 
   profiles <- profiles |>
@@ -32,117 +55,106 @@ condensed_mastery <- function(ratings, profiles) {
     dplyr::arrange(.data$total, dplyr::across(dplyr::everything(),
                                               dplyr::desc))
 
+  pl_dict <- tibble::tibble(pl = pl_labels) |>
+    dplyr::mutate(pl_num = dplyr::row_number())
+
   # prep data
   rf_dat <- ratings |>
-    tidyr::pivot_longer(cols = dplyr::starts_with("rater"),
-                        names_to = "rater_id",
-                        values_to = "rating") |>
-    dplyr::select(-"rater_id") |>
-    dplyr::count(.data$profile_num, !!!rlang::syms(ee_vec), .data$rating) |>
-    dplyr::mutate(rating = dplyr::case_when(rating == 1 ~ "emerging",
-                                            rating == 2 ~ "approaching",
-                                            rating == 3 ~ "target",
-                                            rating == 4 ~ "advanced"),
-                  rating = factor(.data$rating,
-                                  levels = c("emerging", "approaching",
-                                             "target", "advanced"))) |>
+    dplyr::count(.data$profile_num, !!!rlang::syms(att_vec), .data$rating) |>
+    dplyr::left_join(pl_dict, by = c("rating" = "pl_num")) |>
+    dplyr::mutate(rating = .data$pl) |>
+    dplyr::select(-"pl") |>
+    dplyr::mutate(rating = factor(.data$rating,
+                                  levels = pl_labels)) |>
     tidyr::pivot_wider(names_from = "rating", values_from = "n",
                        values_fill = 0L, names_expand = TRUE) |>
     dplyr::rename(profile_id = profile_num) |>
     dplyr::rowwise() |>
-    dplyr::mutate(lls_mastered =
-                    sum(dplyr::c_across(dplyr::starts_with("EE")))) |>
+    dplyr::mutate(atts_mastered =
+                    sum(dplyr::c_across(dplyr::any_of(att_vec)))) |>
     dplyr::ungroup() |>
-    dplyr::select("profile_id", "lls_mastered", "emerging", "approaching",
-                  "target", "advanced")
+    dplyr::select("profile_id", "atts_mastered", dplyr::any_of(pl_labels))
 
   # Fit models -------------------------------------------------------------------
   ## Step 1: Calculate how many panelists put each profile in each PLD or higher
   rf_dat <- rf_dat |>
-    dplyr::rowwise() |>
-    dplyr::mutate(total_ratings =
-                    sum(dplyr::c_across(.data$emerging:.data$advanced),
-                        na.rm = TRUE),
-                  app =
-                    sum(dplyr::c_across(.data$approaching:.data$advanced),
-                        na.rm = TRUE),
-                  tar =
-                    sum(dplyr::c_across(.data$target:.data$advanced),
-                        na.rm = TRUE),
-                  adv =
-                    sum(dplyr::c_across(.data$advanced), na.rm = TRUE)) |>
-    dplyr::ungroup() |>
-    dplyr::select(-c("emerging", "approaching", "target", "advanced"))
+    tidyr::pivot_longer(cols = -c("profile_id", "atts_mastered"),
+                        names_to = "pl", values_to = "num") |>
+    dplyr::left_join(pl_dict, by = c("pl")) |>
+    dplyr::group_by(.data$profile_id) |>
+    dplyr::mutate(total_ratings = sum(.data$num)) |>
+    dplyr::ungroup()
+
+  for (ii in 2:length(pl_labels)) {
+    rf_dat <- rf_dat |>
+      dplyr::group_by(.data$profile_id) |>
+      dplyr::mutate(tmp = dplyr::case_when(.data$pl_num >= ii ~ num,
+                                           TRUE ~ 0),
+                    !!rlang::sym(pl_labels[ii]) := sum(.data$tmp)) |>
+      dplyr::ungroup() |>
+      dplyr::select(-"tmp")
+  }
+
+  rf_dat <- rf_dat |>
+    dplyr::select("profile_id", "atts_mastered", "total_ratings",
+                  dplyr::any_of(pl_labels[2:length(pl_labels)])) |>
+    dplyr::distinct()
 
   ## Step 2: Convert counts to long-format (i.e., 0s and 1s)
   long_rf <- rf_dat |>
-    dplyr::group_by(.data$profile_id, .data$lls_mastered) |>
-    dplyr::reframe(app = c(rep(0L, .data$total_ratings - max(.data$app)),
-                           rep(1L, max(.data$app))),
-                   tar = c(rep(0L, .data$total_ratings - max(.data$tar)),
-                           rep(1L, max(.data$tar))),
-                   adv = c(rep(0L, .data$total_ratings - max(.data$adv)),
-                           rep(1L, max(.data$adv)))) |>
-    dplyr::ungroup()
+    dplyr::select("profile_id", "atts_mastered", "total_ratings") |>
+    tidyr::crossing(poss_ratings = 1:max(.data$total_ratings)) |>
+    dplyr::filter(.data$poss_ratings <= .data$total_ratings) |>
+    dplyr::select(-"total_ratings", -"poss_ratings")
+
+  for (ii in 2:length(pl_labels)) {
+    tmp_rf <- rf_dat |>
+      dplyr::group_by(.data$profile_id, .data$atts_mastered) |>
+      dplyr::reframe(!!rlang::sym(pl_labels[ii]) :=
+                       c(rep(0L,
+                             .data$total_ratings -
+                               max(!!rlang::sym(pl_labels[ii]))),
+                         rep(1L, max(!!rlang::sym(pl_labels[ii]))))) |>
+      dplyr::ungroup() |>
+      dplyr::select(-"profile_id", -"atts_mastered")
+
+    long_rf <- dplyr::bind_cols(long_rf, tmp_rf)
+  }
 
   ## Step 3: Fit a model for each cut point
-  source(here::here("sim-study-4/R/functions.R"))
-
   model_results <- long_rf |>
-    tidyr::pivot_longer(cols = c("app", "tar", "adv"), names_to = "model",
+    tidyr::pivot_longer(cols = dplyr::any_of(pl_labels[2:length(pl_labels)]),
+                        names_to = "model",
                         values_to = "y") |>
-    tidyr::nest(model_dat = c(.data$profile_id, .data$lls_mastered, .data$y)) |>
+    tidyr::nest(model_dat = c("profile_id", "atts_mastered", "y")) |>
     dplyr::mutate(params = purrr::map(model_dat, fit_model)) |>
-    tidyr::unnest(.data$params)
+    tidyr::unnest("params")
 
   # Calculate pinpointing ranges -------------------------------------------------
-  ## Predicted cut points are defined as the value of LLs mastered at the
+  ## Predicted cut points are defined as the value of attributes mastered at the
   ## inflection point of the logistic curve. The x value of the inflection point
   ## is given by -B_0 / B_1.
   pinpoint_ranges <- model_results |>
     dplyr::mutate(predicted = -.data$intercept / .data$slope,
                   range = tidybayes::mean_hdci(.data$predicted,
                                                .width =
-                                                 .data$pinpoint_interval)) |>
-    tidyr::unnest(.data$range) |>
+                                                 pinpoint_interval)) |>
+    tidyr::unnest("range") |>
     dplyr::mutate(.value = round(.data$.value, digits = 0),
                   .lower = floor(.data$.lower),
                   .upper = ceiling(.data$.upper)) |>
     dplyr::rowwise() |>
-    dplyr::mutate(.lower = min(.data$.value - .data$min_pinpoint_range,
+    dplyr::mutate(.lower = min(.data$.value - min_pinpoint_range,
                                .data$.lower),
                   .lower = max(.data$.lower, 0),
-                  .upper = max(.data$.value + .data$min_pinpoint_range,
+                  .upper = max(.data$.value + min_pinpoint_range,
                                .data$.upper),
-                  .upper = min(.data$.upper, num_ees * 4)) |>
+                  .upper = min(.data$.upper,
+                               length(att_vec) * att_levels)) |>
     dplyr::ungroup() |>
     dplyr::select(cut_point = "model", predicted = ".value",
                   pinpoint_min = ".lower", pinpoint_max = ".upper")
 
   pinpoint_ranges |>
     readr::write_csv(glue::glue("{output_dir}/pp-range.csv"))
-
-  pinpoint_possible_profiles <- pinpoint_ranges |>
-    dplyr::mutate(cut_point = factor(.data$cut_point,
-                                     levels = c("app", "tar", "adv"))) |>
-    dplyr::group_by(.data$cut_point) |>
-    tidyr::complete(predicted =
-                      min(.data$pinpoint_min):max(.data$pinpoint_max)) |>
-    dplyr::ungroup() |>
-    dplyr::filter(.data$predicted >= 0 & .data$predicted <= num_ees * 4) |>
-    dplyr::select(lls_mastered = "predicted") |>
-    dplyr::distinct() |>
-    dplyr::left_join(profiles |>
-                       dplyr::select(dplyr::starts_with("EE"), "total") |>
-                       dplyr::anti_join(assigned_profiles,
-                                        by = c(ee_vec, "total")),
-                     by = c("lls_mastered" = "total")) |>
-    dplyr::filter(!is.na(.data$EE_1)) |>
-    dplyr::mutate(emerging = NA_real_, approaching = NA_real_,
-                  target = NA_real_, advanced = NA_real_)
-
-  pinpoint_possible_profiles |>
-    readr::write_csv(glue::glue("{output_dir}/pp.csv"))
-
-  return(pinpoint_possible_profiles)
 }
